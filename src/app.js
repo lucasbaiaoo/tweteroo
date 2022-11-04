c40bca0727b094341f6d655f7995ce78ebf82450
luskamusca <luskamuskammouse@gmail.com>
2022-11-04 12:55:40 -0300

    feat: create POST sign-up

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const user = [];
+
+const tweets = [];
+
+server.post("/sign-up", (req, res) => {
+    user.push(req.body);
+    res.send("OK")
+})
+
+server.listen(5000);
